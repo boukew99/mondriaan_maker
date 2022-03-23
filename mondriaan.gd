d76d2b93efdb45bb15a268d7efaa3d400fd62478
boukew99 <boukewestra0@gmail.com>
2022-03-23 17:10:18 +0100

    no min_size

--- mondriaan.gd
+++ mondriaan.gd
@@ -1,7 +1,6 @@
 extends Control
 
 func _ready():
-	OS.min_window_size = OS.window_size
 	if OS.get_name() == "HTML5": $VBoxContainer/HBoxContainer/Capture.hide() # Saving does not work on HTML5?
 
 func _on_Capture_pressed():
