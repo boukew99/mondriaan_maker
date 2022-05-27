92c4eb54d0dfb1f19a25baef523643c0d565a1f3
boukew99 <boukewestra0@gmail.com>
2022-05-27 14:01:07 +0200

    dynamic splitting

--- color_drop_rect.gd
+++ color_drop_rect.gd
@@ -1,10 +1,18 @@
 extends ColorRect
 
 func can_drop_data(_position, data):
-	return data is Color 
+	return data is Color or data is Array
 
 func drop_data(_position, data):
-	color = data
+	if data is Color:
+		color = data
+	elif data is Array:
+		var split = data[0].instance() if rect_size.aspect() > 1.116481401  else data[1].instance()
+		print(split.name)
+		split.get_child(0).color = color
+		get_parent().add_child(split)
+		get_parent().move_child(split, get_index())
+		queue_free()
 
 func get_drag_data(_position):
 	var preview = ColorRect.new()
--- mondriaan.gd
+++ mondriaan.gd
@@ -6,3 +6,6 @@ func _ready():
 func _on_Capture_pressed():
 	$ScreenCapture.capture_rect = $VBoxContainer/PanelContainer.get_global_rect()
 	$ScreenCapture.popup_centered()
+
+func get_drag_data(position):
+	return [preload("res://h_split.tscn"), preload("res://v_split.tscn")]
